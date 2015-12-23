6e10c62502227e10c75e9181ce783dd90b7989ed
francisvalero <francisvalero@live.com>
2015-12-23 22:52:55 +0800

    -------------------

--- src/main/Problem030.kt
+++ src/main/Problem030.kt
@@ -8,7 +8,7 @@ package main
 fun main(args: Array<String>) {
     val upperBound = 9L.pow(5) * 6
     val range = (2..upperBound)
-    val pow = listOf<Long>(0, 1, 32, 243, 1024, 3125, 7776, 16807, 32768, 59049)
+    val pow = listOf(0L, 1, 32, 243, 1024, 3125, 7776, 16807, 32768, 59049)
     fun Long.fifthPowDigitSum() = this.toDigitsList().map { pow[it.toInt()] }.sum()
     val sum = range.asSequence()
                    .filter { it == it.fifthPowDigitSum() }
